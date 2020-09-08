435b3083f762534b185f95093cda4199c5056919
Fábio Barbosa <fab.rb@outlook.com>
2020-09-08 20:56:19 -0300

    Added score and AI

--- game.cpp
+++ game.cpp
@@ -13,9 +13,12 @@ float arena_half_size_y = 45;
 
 float ball_p_x;
 float ball_p_y;
-float ball_dp_x = 100;
+float ball_dp_x = 180;
 float ball_dp_y;
 float ball_half_size = 1;
+
+int player01_score;
+int player02_score;
 
 internal bool aabb_vs_aabb(float p1x, float p1y, float hs1x, float hs1y,
 	float p2x, float p2y, float hs2x, float hs2y) {
@@ -52,12 +55,31 @@ internal void simulate_game(Input* input, float dt) {
 	float player1_ddp = 0.f;
 	float player2_ddp = 0.f;
 
+#if 0
 	if (is_down(BUTTON_UP)) {
 		player1_ddp += 2000;
 	}
 	if (is_down(BUTTON_DOWN)) {
 		player1_ddp -= 2000;
 	}
+#else
+	/*if ( ball_p_y > player1_p ) {
+		player1_ddp += 1000;
+	}
+	if ( ball_p_y < player1_p ) {
+		player1_ddp -= 1000;
+	}*/
+
+	player1_ddp = (ball_p_y - player1_p) * 90;
+
+	if (player1_ddp > 1200) {
+		player1_ddp = 1200;
+	}
+	if (player1_ddp < -1200) {
+		player1_ddp = -1200;
+	}
+
+#endif
 
 	if (is_down(BUTTON_W)) {
 		player2_ddp += 2000;
@@ -104,14 +126,21 @@ internal void simulate_game(Input* input, float dt) {
 			ball_dp_y = 0;
 			ball_p_x = 0;
 			ball_p_y = 0;
+
+			player01_score++;
 		}
 		else if (ball_p_x - ball_half_size < -arena_half_size_x) {
 			ball_dp_x *= -1;
 			ball_dp_y = 0;
 			ball_p_x = 0;
 			ball_p_y = 0;
+
+			player02_score++;
 		}
 	}
+
+	draw_number(player01_score, -10, 40, 1.f, 0xbbffbb);
+	draw_number(player02_score, 10, 40, 1.f, 0xbbffbb);
 
 	draw_rect(ball_p_x, ball_p_y, 1, 1, 0xffffff);
 
